e390df33fb37db67d5f94f3f407f94ce3769a5b3
Damian Stasik <visualfanatic@users.noreply.github.com>
2018-05-16 17:29:19 +0200

    Adjust validation for final-form v4

--- src/utils.js
+++ src/utils.js
@@ -2,5 +2,11 @@ export const getChildren = children => {
   return Array.isArray(children) ? children : [children]
 }
 
-export const composeValidators = validators => (...args) =>
+const composeValidators = (validators, ...args) =>
   validators.reduce((error, validator) => error || validator(...args), undefined)
+
+export const composeFormValidators = validators => (...args) =>
+  composeValidators(validators, ...args)
+
+export const composeFieldValidators = validators => () => (...args) =>
+  composeValidators(validators, ...args)
