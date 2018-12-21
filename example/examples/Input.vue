e2cb49bae735c05aca85236fedfaaca4c4f0fba6
Alireza Valizade <alirezavalizade0@gmail.com>
2018-12-21 16:20:42 +0100

    add Input components

---
+++ example/examples/Input.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input v-on="fieldProps.events" :name="fieldProps.name" :value="fieldProps.value" type="email">
+    <span
+      class="error"
+      v-if="fieldProps.meta.touched && fieldProps.meta.error"
+    >{{ fieldProps.meta.error }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    fieldProps: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
