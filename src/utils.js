d099df1bb861f1d39e61b6972616ffc30e071ae2
Artem Petrenkov <static.lab@gmail.com>
2021-12-05 19:26:01 +0300

    fix eslint warnings

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@ export const getChildren = children =>
   Array.isArray(children) ? children : [children]
 
 const composeValidators = (validators, ...args) =>
+  // eslint-disable-next-line unicorn/no-array-reduce
   validators.reduce((error, validator) => error || validator(...args), undefined)
 
 export const composeFormValidators = validators => (...args) =>
@@ -10,10 +11,6 @@ export const composeFormValidators = validators => (...args) =>
 export const composeFieldValidators = validators => () => (...args) =>
   composeValidators(validators, ...args)
 
-export const makeSubscriptionObject = subscriptionItems => subscriptionItems.reduce(
-  (result, key) => {
-    result[key] = true
-    return result
-  },
-  {},
-)
+export const makeSubscriptionObject = subscriptionItems =>
+  // eslint-disable-next-line no-use-extend-native/no-use-extend-native
+  Object.fromEntries(subscriptionItems.map(key => [key, true]))
